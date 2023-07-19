54ef078694ffb7bafaadde42e486cd5866b6afe1
aleksanderinglot <aleksanderinglot@hotmail.com>
2023-07-19 10:31:57 +0200

    Task solution - frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { CurrencyFormComponent } from './currency-form/currency-form.component';
+import { RequestListComponent } from './request-list/request-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CurrencyFormComponent },
+  { path: 'requests', component: RequestListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrencyFormComponent } from './currency-form/currency-form.component';
+import { RequestListComponent } from './request-list/request-list.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyFormComponent,
+    RequestListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
